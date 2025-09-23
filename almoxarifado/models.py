3c0993f6284f67243310ba0f25b26b4faa23277b
Rodrigo Lemos <rcamilodelemos@gmail.com>
2025-09-23 14:35:36 -0300

    start project

---
+++ almoxarifado/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Instituicao(models.Model):
+    nome = models.CharField(max_length=100)
+    cep = models.CharField(max_length=9)
+    logradouro = models.CharField(max_length=200)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    telefone = models.CharField(max_length=20)
+    cnpj = models.CharField(max_length=18, unique=True)
+    
+class Funcionario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=20)
+    instituicao = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
